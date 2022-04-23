117fc1f8d0daca7e106d55bb5e543b670f74c731
sammyalwayscode <olorundasamuel2@gmail.com>
2022-04-23 17:32:57 +0100

    SnapShot

---
+++ src/Components/Post/Contact.js
@@ -0,0 +1,127 @@
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { db } from "../Base/Base";
+import randomColor from "randomcolor";
+
+const Contact = () => {
+  const color = randomColor();
+  const [users, setUsers] = useState([]);
+  const [snapUser, setSnapUser] = useState([]);
+
+  const getData = async () => {
+    const data = await getDocs(collection(db, "contactUsers"));
+    setUsers(data.docs.map((el) => ({ ...el.data(), id: el.id })));
+  };
+
+  const snapGet = async () => {
+    const user = collection(db, "contactUsers");
+    onSnapshot(user, (snapshot) => {
+      const r = [];
+      snapshot.forEach((doc) => {
+        r.push({ ...doc.data(), id: doc.id });
+        console.log(r);
+      });
+      setSnapUser(r);
+    });
+  };
+
+  console.log(users);
+
+  useEffect(() => {
+    getData();
+    snapGet();
+  }, []);
+
+  return (
+    <Container>
+      <Wrapper>
+        {snapUser?.map((props) => (
+          <ContactHold key={props.id}>
+            <FirstLetter
+              style={{ backgroundColor: `${color}` }}
+              to={`/${props.id}`}
+            >
+              <span> {props.firstName.charAt()} </span>
+            </FirstLetter>
+            <FirstName> {props.firstName} </FirstName>
+            <LastName> {props.lastName} </LastName>
+            <OthersHold>
+              <Company> {props.userCompany} </Company>
+              <PhoneNo> {props.userPhoneNo} </PhoneNo>
+            </OthersHold>
+          </ContactHold>
+        ))}
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Contact;
+
+const Container = styled.div`
+  height: 100%;
+  min-height: 90vh;
+  background-color: #eee;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  font-family: poppins;
+  border-radius: 9px;
+`;
+const Wrapper = styled.div`
+  height: 100%;
+  min-height: 400px;
+  background-color: #fff;
+  width: 800px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* justify-content: center; */
+`;
+const ContactHold = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 90%;
+  /* background-color: gold; */
+  /* box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px,
+    rgba(0, 0, 0, 0.06) 0px 2px 4px -1px; */
+  box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px,
+    rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
+  min-height: 65px;
+  height: 100%;
+  border-radius: 4px;
+  justify-content: space-around;
+  align-items: center;
+  margin: 10px 0;
+`;
+const FirstLetter = styled(Link)`
+  text-decoration: none;
+  height: 50px;
+  width: 50px;
+  /* background-color: #f4b400; */
+  color: #fff;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 25px;
+  font-weight: 700;
+`;
+const FirstName = styled.div`
+  font-weight: 600;
+`;
+const LastName = styled.div``;
+const OthersHold = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Company = styled.div`
+  font-size: small;
+  font-weight: 600;
+`;
+const PhoneNo = styled.div``;
---
+++ src/Edit.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { db } from "../Firebase/Base";
+
+import {
+    collection,
+    deleteDoc,
+    doc,
+    onSnapshot,
+    updateDoc,
+  } from "firebase/firestore";
+
+
+
+const Edit = () => {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const editData = async (id, newName, newEmail) => {
+    const userDoc = doc(db, "shotNameColl", id);
+    const newField = { fusr: newName, newEmail: newEmail };
+    await updateDoc(userDoc, newField);
+  };
+
+
+
+  return (
+    <div>
+      <input
+        placeholder="firstName"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <input
+        placeholder="lastName"
+        value={lastName}
+        onChange={(e) => {
+          setLastName(e.target.value);
+        }}
+      />
+      <button>Submit</button>
+    </div>
+  );
+};
+
+export default Edit;
